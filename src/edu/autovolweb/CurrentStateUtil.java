7a219941bb97de058dd53f5a70d96d55d2234e9a
Derek Salama <dereksalama@gmail.com>
2014-03-26 00:09:56 -0400

    gm training

---
+++ src/edu/autovolweb/CurrentStateUtil.java
@@ -0,0 +1,94 @@
+package edu.autovolweb;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import com.autovol.CurrentStateData;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+// Use this to convert to Instance so we don't need Weka lib on android
+public class CurrentStateUtil {
+
+	private static Collection<CurrentStateData> fromJson(String json) {
+		Gson gson = new Gson();
+		Type collectionType = new TypeToken<Collection<CurrentStateData>>(){}.getType();
+		Collection<CurrentStateData> data = gson.fromJson(json, collectionType);
+		return data;
+	}
+	
+	private static void addInstance(CurrentStateData state, Instances dataset) {
+		Instance i = new DenseInstance(CurrentStateData.NUM_ATTRS);
+		i.setDataset(dataset);
+		
+		i.setValue(dataset.attribute("time"), state.getTime());
+		i.setValue(dataset.attribute("lat"), state.getLat());
+		i.setValue(dataset.attribute("lon"), state.getLon());
+		i.setValue(dataset.attribute("loc_provider"), state.getLocProvider());
+		i.setValue(dataset.attribute("light"), state.getLight());
+		i.setValue(dataset.attribute("distance"), state.getLat());
+		i.setValue(dataset.attribute("wifi_count"), state.getWifiCount());
+		i.setValue(dataset.attribute("charging"), state.getCharging());
+		i.setValue(dataset.attribute("activity_type"), state.getActivityType());
+		i.setValue(dataset.attribute("activityConfidence"), state.getActivityConfidence());
+		i.setValue(dataset.attribute("ringer"), state.getRinger());
+		dataset.add(i);
+	}
+	
+	public static Instances convertCurrentStateData(String json) {
+		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+		
+		attributes.add(new Attribute("time"));
+		attributes.add(new Attribute("lat"));
+		attributes.add(new Attribute("lat"));
+		
+		ArrayList<String> locProviderValues = new ArrayList<String>();
+		locProviderValues.add("gps");
+		locProviderValues.add("network");
+		locProviderValues.add("fused");
+		attributes.add(new Attribute("loc_provider", locProviderValues));
+		
+		attributes.add(new Attribute("light"));
+		attributes.add(new Attribute("distance"));
+		attributes.add(new Attribute("wifi_count"));
+		
+		ArrayList<String> chargingValues = new ArrayList<String>();
+		chargingValues.add("true");
+		chargingValues.add("false");
+		attributes.add(new Attribute("charging", chargingValues));
+		
+		ArrayList<String> activityValues = new ArrayList<String>();
+		chargingValues.add("activity_vehicle");
+		chargingValues.add("activity_bike");
+		chargingValues.add("activity_foot");
+		chargingValues.add("activity_still");
+		chargingValues.add("activity_unknown");
+		chargingValues.add("activity_tilting");
+		attributes.add(new Attribute("activity_type", activityValues));
+		
+		attributes.add(new Attribute("activity_confidence"));
+		
+		ArrayList<String> ringerValues = new ArrayList<String>();
+		ringerValues.add("silent");
+		ringerValues.add("vibrate");
+		ringerValues.add("normal");
+		Attribute ringerAttr = new Attribute("ringer", ringerValues);
+		attributes.add(ringerAttr);
+		
+		Instances data = new Instances("Training", attributes, 0);
+		data.setClass(ringerAttr);
+		
+		Collection<CurrentStateData> states = fromJson(json);
+		for (CurrentStateData state : states) {
+			addInstance(state, data);
+		}
+		
+		return data;
+	}
+}
---
+++ src/edu/autovolweb/DataUploadServlet.java
@@ -0,0 +1,164 @@
+package edu.autovolweb;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
+
+import weka.clusterers.EM;
+import weka.clusterers.FilteredClusterer;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.filters.unsupervised.attribute.Normalize;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class DataUploadServlet
+ */
+@WebServlet("/DataUploadServlet")
+public class DataUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final int DATA_AGE = 14; //keep data for two weeks
+	
+	public static final String CLUSTER_LABELS_FILE = "cluster_labels";
+	public static final String EM_MODEL_FILE = "em_model";
+    
+	private ExecutorService executor;
+	
+	 @Override
+	 public void init() throws ServletException {
+	    	executor = Executors.newCachedThreadPool();
+	 }
+	 
+	 @Override
+	 public void destroy() {
+	    	try {
+				executor.awaitTermination(5, TimeUnit.MINUTES);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	 }
+	 
+	 private class RetrainRunnable implements Runnable {
+		 private final String newData;
+		 public RetrainRunnable(String newData) {
+			this.newData = newData;
+		}
+		 
+		 @Override
+		 public void run() {
+			 // interpret incoming data
+			 Instances incomingData = CurrentStateUtil.convertCurrentStateData(newData);
+
+			 // write to today's file
+			 DateTime today = new DateTime();
+			 String newFileName = "data_" + today.getDayOfMonth() + "_" +
+					 today.getMonthOfYear() + "_" + today.getYear();
+
+			 File newFile = new File(newFileName);
+			 ArffSaver saver = new ArffSaver();
+			 saver.setInstances(incomingData);
+			 try {
+				saver.setFile(newFile);
+				saver.writeBatch();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			 
+			 Instances allData = new Instances(incomingData);
+			 allData.setClassIndex(allData.numAttributes() - 1);
+
+			 // load previous data (up to how old?)
+			 for (int i = 1; i < DATA_AGE; i++) {
+				 DateTime day = today.minusDays(i);
+				 String fileName = "data_" + day.getDayOfMonth() + "_" +
+						 day.getMonthOfYear() + "_" + day.getYear();
+				 File f = new File(fileName);
+				 if (f.exists()) {
+					 try {
+						BufferedReader reader = new BufferedReader(new FileReader(f));
+						Instances moreData = new Instances(reader);
+						allData.addAll(moreData);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				 } else {
+					 break; // have gone past oldest file
+				 }
+			 }
+
+			 // EM cluster
+			 EM unfilteredEM = new EM();
+			 unfilteredEM.setMaximumNumberOfClusters(20);
+			 Normalize normalizer = new Normalize();
+			 normalizer.setIgnoreClass(true);
+
+
+			 try {
+				 normalizer.setInputFormat(allData);
+				 FilteredClusterer em = new FilteredClusterer();
+				 em.setClusterer(unfilteredEM);
+				 em.setFilter(normalizer);
+				 em.buildClusterer(allData);
+				 List<EMCluster> clusterToLabels = EMCluster
+						 .createClusterToLabelMap(allData, em);
+				 
+				 
+				 // save cluster labels
+				 File clusterToLabelsFile = new File(CLUSTER_LABELS_FILE);
+				 BufferedWriter writer = new BufferedWriter(
+						 new FileWriter(clusterToLabelsFile, false)); // overwrite
+				 
+				 Gson gson = new Gson();
+				 String json = gson.toJson(clusterToLabels);
+				 writer.write(json);
+				 writer.close();
+				 
+				 
+				 // save EM model
+				 ObjectOutputStream oos = new ObjectOutputStream(
+						 new FileOutputStream(EM_MODEL_FILE, false));
+				 oos.writeObject(em);
+				 oos.flush();
+				 oos.close();
+			 } catch (Exception e) {
+				 e.printStackTrace();
+			 }
+		 }
+
+	 }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		final String incomingDataString = request.getParameter("data");
+		executor.submit(new RetrainRunnable(incomingDataString));
+
+		response.setStatus(HttpServletResponse.SC_ACCEPTED);
+	}
+
+}
