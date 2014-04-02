cb0e0a2d7ab1ba420084cf40ed8eba4efaf1af37
Derek Salama <dereksalama@gmail.com>
2014-04-02 17:05:28 -0400

    prob of label fix

--- src/edu/autovolweb/EMCluster.java
+++ src/edu/autovolweb/EMCluster.java
@@ -1,8 +1,6 @@
 package edu.autovolweb;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import weka.clusterers.FilteredClusterer;
 import weka.core.Instance;
@@ -12,14 +10,24 @@ public class EMCluster {
 	
 	/* Static util methods */
 	public static int findMaxCluster(double[] distribs) {
+		/*
 		 List<Double> distribsList = new ArrayList<Double>(distribs.length);
 		 for (int j = 0; j < distribs.length; j++) {
 			 distribsList.add(distribs[j]);
 		 }
 		 Double maxVal = Collections.max(distribsList);
 		 int maxCluster = distribsList.indexOf(maxVal);
-		 
-		 return maxCluster;
+		 */
+		int maxInd = 0;
+		double maxVal = -1;
+		for (int i = 0; i < distribs.length; i++) {
+			if (distribs[i] > maxVal) {
+				maxInd = i;
+				maxVal = distribs[i];
+			}
+		}
+
+		return maxInd;
 	}
 	
 	public static ArrayList<EMCluster> createClusterToLabelMap(Instances labeledData, Instances unlabeledData, FilteredClusterer em) {
@@ -42,6 +50,7 @@ public class EMCluster {
 			 ArrayList<EMCluster> clusterToLabelMap = 
 					 new ArrayList<EMCluster>(em.numberOfClusters());
 			 for (int i = 0; i < em.numberOfClusters(); i++) {
+				 /*
 				 List<Integer> countList = new ArrayList<Integer>(labeledData.numClasses());
 				 long totalCount = 0;
 				 for (int j = 0; j < clusterLabelCounts[i].length; j++) {
@@ -51,14 +60,26 @@ public class EMCluster {
 				 
 				 Integer maxCount = Collections.max(countList);
 				 int maxLabel = countList.indexOf(maxCount);
-				 String maxLabelString = labeledData.classAttribute().value(maxLabel);
+				 */
+				 int totalCount = 0;
+				 int maxCount = Integer.MIN_VALUE;
+				 int maxInd = 0;
+				 for (int j = 0; j < em.numberOfClusters(); j++) {
+					 int currentCount = clusterLabelCounts[i][j];
+					 totalCount += currentCount;
+					 if (currentCount > maxCount) {
+						 maxInd = j;
+						 maxCount = currentCount;
+					 }
+				 }
+				 String maxLabelString = labeledData.classAttribute().value(maxInd);
 				 EMCluster cluster = new EMCluster();
 				 cluster.setRingerLabel(maxLabelString);
 				 cluster.setCluster(i);
-
-		
-				 double probOfLabel = maxCount / totalCount;
+				 
+				 double probOfLabel = ((double) maxCount) / totalCount; // force floating point
 				 cluster.setProbOfLabel(probOfLabel);
+				 
 				 clusterToLabelMap.add(i, cluster);
 			 }
 			 
